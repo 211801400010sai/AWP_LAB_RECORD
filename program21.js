c58f78c75ab3acea99a36b3b89f19f7665ca996b
211801400010sai <134459013+211801400010sai@users.noreply.github.com>
2023-05-24 09:40:06 +0530

    Add files via upload

---
+++ program21.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#get-data").click(function() {
+        $.ajax({
+            url: "https://jsonplaceholder.typicode.com/posts",
+            method: "GET",
+            dataType: "json",
+            success: function(result) {
+                var data = "";
+                $.each(result, function(index, post) {
+                    data += "<h2>" + post.title + "</h2><p>" + post.body + "</p>";
+                });
+                $("#data-container").html(data);
+            }
+        });
+    });
+});
